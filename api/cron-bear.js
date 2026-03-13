7a75756f707163635570e4233a268e6ac95a50e4
franselsimmons <lrsdaktechniek@hotmail.com>
2026-03-13 11:26:54 +0100

    Update cron-bear.js

--- api/cron-bear.js
+++ api/cron-bear.js
@@ -2,24 +2,11 @@
 import mainScan from "./scan.js";
 
 export default async function handler(req, res) {
-  try {
-    req.query = {
-      ...(req.query || {}),
-      mode: "bear",
-      token: process.env.CRON_SECRET,
-    };
+  req.query = {
+    ...(req.query || {}),
+    mode: "bear",
+    token: process.env.CRON_SECRET,
+  };
 
-    req.headers = {
-      ...(req.headers || {}),
-      authorization: `Bearer ${process.env.CRON_SECRET || ""}`,
-    };
-
-    return await mainScan(req, res);
-  } catch (e) {
-    console.error("api/cron-bear.js error:", e);
-    return res.status(500).json({
-      ok: false,
-      error: e?.message || "cron bear failed",
-    });
-  }
+  return mainScan(req, res);
 }
