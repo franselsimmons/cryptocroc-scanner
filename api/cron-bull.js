6b139a9c7ca419eba679a78b85a22e170d62ce6e
franselsimmons <lrsdaktechniek@hotmail.com>
2026-03-13 11:26:33 +0100

    Update cron-bull.js

--- api/cron-bull.js
+++ api/cron-bull.js
@@ -2,24 +2,11 @@
 import mainScan from "./scan.js";
 
 export default async function handler(req, res) {
-  try {
-    req.query = {
-      ...(req.query || {}),
-      mode: "bull",
-      token: process.env.CRON_SECRET,
-    };
+  req.query = {
+    ...(req.query || {}),
+    mode: "bull",
+    token: process.env.CRON_SECRET,
+  };
 
-    req.headers = {
-      ...(req.headers || {}),
-      authorization: `Bearer ${process.env.CRON_SECRET || ""}`,
-    };
-
-    return await mainScan(req, res);
-  } catch (e) {
-    console.error("api/cron-bull.js error:", e);
-    return res.status(500).json({
-      ok: false,
-      error: e?.message || "cron bull failed",
-    });
-  }
+  return mainScan(req, res);
 }
