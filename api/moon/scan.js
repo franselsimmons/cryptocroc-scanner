866af4b5dc5862cb9e4157ad4261268ff8a37eb4
franselsimmons <lrsdaktechniek@hotmail.com>
2026-03-13 11:15:10 +0100

    Update scan.js

--- api/moon/scan.js
+++ api/moon/scan.js
@@ -1482,6 +1482,5 @@ export default async function handler(req, res) {
     console.error("Moon scan error:", err);
     res.status(500).json({ ok: false, error: err.message });
   } finally {
-    if (lockAcquired) await releaseScanLock(mode);
   }
 }
