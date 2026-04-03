a3dcb124a9b427c761c8b0fe6ad0d857d4f78659
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-03 09:54:12 +0200

    Update cron-bear.js

--- api/cron-bear.js
+++ api/cron-bear.js
@@ -2,11 +2,9 @@
 import mainScan from "./scan.js";
 
 export default async function cronBear(req, res) {
-  req.query = {
-    ...(req.query || {}),
-    mode: "bear",
-    token: process.env.CRON_SECRET,
-  };
+  req.query = req.query || {};
+  req.query.mode = "bear";
+  req.query.token = process.env.CRON_SECRET;
 
   return mainScan(req, res);
 }
