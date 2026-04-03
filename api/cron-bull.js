d4a2d56abf55cff6a7119b238f516292ee5fb28e
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-03 09:53:57 +0200

    Update cron-bull.js

--- api/cron-bull.js
+++ api/cron-bull.js
@@ -2,11 +2,9 @@
 import mainScan from "./scan.js";
 
 export default async function cronBull(req, res) {
-  req.query = {
-    ...(req.query || {}),
-    mode: "bull",
-    token: process.env.CRON_SECRET,
-  };
+  req.query = req.query || {};
+  req.query.mode = "bull";
+  req.query.token = process.env.CRON_SECRET;
 
   return mainScan(req, res);
 }
